9227323c491870edda3c26af9428ff5ff89046b4
hema gopisetty <hemagopisetty2@gmail.com>
2022-01-03 19:02:12 +0530

    commit

--- server.js
+++ server.js
@@ -16,16 +16,9 @@ app.use("/api/users/", userRoute);
 app.use("/api/orders/", ordersRoute);
 
 
-if(process.env.NODE_ENV ==='production')
-{
-    app.use('/' , express.static('client/build'))
-
-    app.get('*' , (req , res)=>{
-
-        res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
-
-    })
-}
+app.use('/',(req,res)=>{
+    res.send("server running in backend")
+})
 
 const port = process.env.PORT || 8000;
 
