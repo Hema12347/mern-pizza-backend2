7eafe417db2c480e0b8dd2f2efa8d5f46b974be7
hema gopisetty <hemagopisetty2@gmail.com>
2022-01-03 18:49:07 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+var mongoURL =
+  "mongodb+srv://hema:hema12@cluster0.dcclt.mongodb.net/mern-pizza";
+
+mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+db.on("connected", () => {
+  console.log(`Mongo DB Connection Successfull`);
+});
+
+db.on("error", () => {
+  console.log(`Mongo DB Connection failed`);
+});
+
+module.exports = mongoose;
